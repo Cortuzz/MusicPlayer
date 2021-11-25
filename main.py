74ad6fd44915ed955fb01b47b69b4c4bbad964c2
Oleg <52497929+Cortuzz@users.noreply.github.com>
2021-11-25 17:06:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,13 @@
 import sys
 import pygame
 
-
-class GUI:
-	def __init__(self, width, height):
-		pygame.font.init()
-		pygame.init()
-		pygame.display.set_caption("Music Player")
-		self.screen = pygame.display.set_mode((width, height))
-
-	def screen_update(self):
-		pygame.display.update()
-		self.screen.fill((0, 0, 0))
+from gui import GUI
 
 
 running = True
 WIDTH = 600
 HEIGHT = 800
 gui = GUI(WIDTH, HEIGHT)
-
 
 if __name__ == '__main__':
 	while running:
