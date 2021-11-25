2a5c14adacce21820adfdf686d242c9980874988
Cortuzz <oleg.cortuzz1@gmail.com>
2021-11-25 21:42:47 +0700

    added play / change volume / mute

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 import sys
 import os
 import pygame
-import player
+
+from player import Player
 from gui import GUI
 
 for root, dirs, files in os.walk("music"):
     for filename in files:
         print(filename[-4:])
 
+
+DIR = os.path.dirname(os.path.abspath(__file__)) + "\music\\"
+WIDTH = 800
+HEIGHT = 500
+
 running = True
-WIDTH = 600
-HEIGHT = 800
 gui = GUI(WIDTH, HEIGHT)
+player = Player(DIR)
+player.load_track('Vitality.mp3')
 
 if __name__ == '__main__':
-	while running:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-		gui.screen_update()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and not player.playing:
+                    player.playing = True
+                    player.play_track()
+                if event.key == pygame.K_UP:
+                    player.change_volume(0.05)
+                if event.key == pygame.K_DOWN:
+                    player.change_volume(-0.05)
+                if event.key == pygame.K_m:
+                    player.change_mute()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    player.change_volume(0.015)
+
+                elif event.button == 5:
+                    player.change_volume(-0.015)
+
+        gui.screen_update()
--- player.py
+++ player.py
@@ -1,6 +1,27 @@
+import pygame
+
+
 class Player:
-    def __init__(self):
-        pass
+    def __init__(self, dir):
+        self.dir = dir
+        self.volume = 0.2
+        self.playing = False
+        self.muted = False
 
     def load_track(self, name):
-        pass
+        pygame.mixer.music.load(self.dir + name)
+
+    def play_track(self):
+        pygame.mixer.music.play()
+
+    def change_volume(self, difference):
+        self.volume =min(max(self.volume + difference, 0), 1)
+        pygame.mixer.music.set_volume(self.volume)
+
+
+    def change_mute(self):
+        if self.muted:
+            pygame.mixer.music.set_volume(self.volume)
+            return
+
+        pygame.mixer.music.set_volume(0)
